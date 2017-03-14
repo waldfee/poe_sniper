f1e11e4ff7f0105511273cf2da61a83b14b84dcc
TheSmellOfVictory <jhoffman590@gmail.com>
2017-03-14 01:54:17 -0500

    Converted to Python and supports multiple searches

--- README.md
+++ README.md
@@ -1,40 +1,26 @@
 # PoE Sniper
-
-A democratic PoE trade sniping tool using GNU Bash scripting.
-
-##Requirements
-A system able to run GNU Bash scripts.
-* Most Linux distros
-* macOS
-* Windows with the [Linux Subsystem](https://msdn.microsoft.com/en-us/commandline/wsl/install_guide) installed.
+A democratic PoE trade sniping tool using Python.
 
 ##Installation
-* Extract the zip to a folder.
-* Launch your Bash shell.
-* Install missing packages from your package manager `sudo apt-get install beep`
-* `cd` into the folder `cd /mnt/c/Users/<username>/Documents/poe_sniper`
-* execute command `./poe_sniper args...`
+* Install [Python 3.6](https://www.python.org/downloads/release/python-360/).
+* Install [Visual C++ 2015 Build Tools](http://landinghub.visualstudio.com/visual-cpp-build-tools)
+* Open a terminal (i.e. Powershell) with Administrator privledges.
+* Install missing packages from your package manager `pip install ujson`
+* Extract poe_sniper to a folder.
+* `cd` into the folder `cd C:\Users\<username>\Documents`
+* execute command `./poe_sniper`
 
 ##Usage
-```
-./poe_sniper <item> <league> <(optional)max currency quantity> <(optional)currency type>
-```
+Edit the searches.json for your search.
+* Everything is case sensitive.
+* See the searches.json for examples.
+Execute poe_sniper `./poe_sniper`
+CTRL + C to end the search.
 
-Example 1:
-```
-./poe_sniper 'Jewel' 'Standard'
-```
-Will look for any item name or type containing the string 'Jewel' in Standard at any price.
-
-Example 2:
-```
-./poe_sniper 'Ancient Rel' 'Hardcore Standard' 5 chaos
-```
-Will look for any item name or type containing the string 'Ancient Rel' in the league matching 'Hardcore Standard' that is under 5 chaos orbs in price.
-
-###Uses
-
-* [jq](https://stedolan.github.io/jq/)
+###Required
+* [Python 3.6](https://www.python.org/downloads/release/python-360/)
+* [ujson](https://pypi.python.org/pypi/ujson) `pip install ujson`
+* [Visual C++ 2015 Build Tools](http://landinghub.visualstudio.com/visual-cpp-build-tools)
 
 ##Donate
 [![paypal](https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif)](https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=7VZ7G7A8ARQHE&lc=US&item_name=PoE%20Sniper&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted)
---
+++ poe_sniper.py
@@ -0,0 +1,124 @@
+"""Imports"""
+import threading
+import time
+import re
+from urllib.request import urlopen
+
+import ujson
+
+
+def check_item(item, tab, search_item):
+    """Checks item against search_item."""
+
+    item_name = item['name']
+    item_typeline = item['typeLine']
+    league = item['league']
+    note = item.get('note')
+    x_coord = str(item['x'])
+    y_coord = str(item['y'])
+
+    if search_item['league'] != league:
+        return False
+
+    if search_item['name'] in item_name:
+        name = re.sub('<.*>', '', item_name)
+    elif search_item['name'] in item_typeline:
+        name = re.sub('<.*>', '', item_typeline)
+    else:
+        return False
+
+    price = note if note else tab
+
+    if bool(re.search(r'^~(price|b\/o) [0-9]+ [a-z]+$', price)):
+        price_parts = price.split(' ')
+        price_quantity = int(price_parts[1])
+        price_type = price_parts[2]
+        price_string = str(price_quantity) + " " + price_type
+    else:
+        return False
+
+    if (search_item['price_type'] and
+            search_item['price_type'] != price_type):
+        return False
+
+    if (search_item['price_type'] and
+            search_item['price_quantity'] and
+            search_item['price_quantity'] < price_quantity):
+        return False
+
+    ret = ujson.dumps(
+        {
+            "name": name,
+            "price": price_string,
+            "league": league,
+            "x": x_coord,
+            "y": y_coord
+        })
+    return ret
+
+
+def parse_stashes(stashes, searches):
+    """Parses stashes."""
+
+    for stash in stashes:
+        character_name = stash['lastCharacterName']
+        tab = stash['stash']
+        items = stash['items']
+
+        for item in items:
+            for search in searches:
+                found_item = check_item(item, tab, search)
+                if found_item:
+                    break
+
+            if not found_item:
+                continue
+
+            found_item = ujson.loads(found_item)
+            print(
+                found_item['price'], "\t\t\t"
+                "@" + character_name,
+                "I would like to buy your", found_item['name'],
+                "listed for", found_item['price'],
+                "in", found_item['league'],
+                "(tab:" + tab, found_item['x'] + "x," + found_item['y'] + "y" + ").")
+            print('\a', end="")
+
+
+def live_indexing(change_id, searches):
+    """Indexing from current change_id."""
+
+    start_time = time.time()
+
+    url = 'http://api.pathofexile.com/public-stash-tabs?id=' + change_id
+    request = urlopen(url)
+    data = ujson.loads(request.read())
+
+    new_thread = threading.Thread(
+        target=parse_stashes, args=(data['stashes'], searches))
+    new_thread.start()
+
+    download_duration = (time.time() - start_time)
+    if download_duration < 1.0:
+        time.sleep(1.0 - download_duration)
+
+    live_indexing(data['next_change_id'], searches)
+
+
+def main():
+    """Main"""
+
+    with open('searches.json') as searches_file:
+        searches_data = ujson.loads(searches_file.read())
+
+    searches = searches_data['searches']
+
+    url = 'http://api.poe.ninja/api/Data/GetStats'
+    request = urlopen(url)
+    data = ujson.loads(request.read())
+
+    live_indexing(data['nextChangeId'], searches)
+
+
+if __name__ == "__main__":
+    main()
